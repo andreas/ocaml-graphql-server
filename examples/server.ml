9988eb0ad2493097ab5912e4f6f5390f462bb939
Andreas Garnaes <andreas.garnaes@gmail.com>
2016-12-03 13:33:55 +0100

    Update example server.

--- examples/server.ml
+++ examples/server.ml
@@ -23,16 +23,19 @@ let user = Schema.(obj
   ~fields:[
     field
       ~name:"id"
+      ~args:Arg.[]
       ~typ:(non_null int)
       ~resolve:(fun () p -> p.id)
     ;
     field
       ~name:"name"
+      ~args:Arg.[]
       ~typ:(non_null string)
       ~resolve:(fun () p -> p.name)
     ;
     field
       ~name:"role"
+      ~args:Arg.[]
       ~typ:(non_null role)
       ~resolve:(fun () p -> p.role)
   ]
@@ -42,10 +45,30 @@ let schema = Schema.(schema
     ~fields:[
       field
         ~name:"users"
+        ~args:Arg.[]
         ~typ:(non_null (list (non_null user)))
         ~resolve:(fun () () -> users)
+      ;
+      field
+        ~name:"greeter"
+        ~typ:string
+        ~args:Arg.[
+          arg "config" ~typ:(non_null (obj ~name:"greeter_config" ~coerce:(fun greeting name -> (greeting, name)) ~fields:[
+            arg "greeting" ~typ:(non_null string);
+            arg "name" ~typ:(non_null string)
+          ]))
+        ]
+        ~resolve:(fun () () (greeting, name) ->
+          Some (Format.sprintf "%s, %s" greeting name)
+        )
+      ;
     ]
 )
+
+let execute query =
+  let open Rresult in
+  Graphql.Parser.parse query >>| fun doc ->
+  Graphql.execute schema () doc
 
 let callback conn (req : Cohttp.Request.t) body =
   Lwt_io.printf "Req: %s\n" req.resource;
@@ -57,12 +80,12 @@ let callback conn (req : Cohttp.Request.t) body =
       Lwt_io.printf "Body: %s\n" query_json;
       let query = Yojson.Basic.from_string query_json |> Yojson.Basic.Util.member "query" |> Yojson.Basic.Util.to_string in
       Lwt_io.printf "Query: %s\n" query;
-      match Graphql.Parser.parse query with
-      | Ok doc ->
-          let data = Graphql.execute schema () doc in
-          let rsp = `Assoc [("data", data)] |> Yojson.Basic.to_string in
+      match execute query with
+      | Ok data ->
+          let rsp = Yojson.Basic.to_string data in
           C.Server.respond_string ~status:`OK ~body:rsp ()
-      | Error err -> C.Server.respond_string ~status:`Internal_server_error ~body:err ()
+      | Error err ->
+          C.Server.respond_string ~status:`Internal_server_error ~body:err ()
     end
   | _ -> C.Server.respond_string ~status:`Not_found ~body:"" ()
 
