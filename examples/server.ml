b809b86a1006ceb1c3f8260e635172092b98714a
Andreas Garnaes <andreas.garnaes@gmail.com>
2017-04-14 13:11:24 +0200

    Fix example server

--- examples/server.ml
+++ examples/server.ml
@@ -44,26 +44,25 @@ let user = Schema.(obj "user"
   ])
 )
 
-let schema = Schema.(schema 
-    ~fields:[
-      io_field "users"
-        ~args:Arg.[]
-        ~typ:(non_null (list (non_null user)))
-        ~resolve:(fun () () -> Lwt.return users)
-      ;
-      field "greeter"
-        ~typ:string
-        ~args:Arg.[
-          arg "config" ~typ:(non_null (obj "greeter_config" ~coerce:(fun greeting name -> (greeting, name)) ~fields:[
-            arg' "greeting" ~typ:string ~default:"hello";
-            arg "name" ~typ:(non_null string)
-          ]))
-        ]
-        ~resolve:(fun () () (greeting, name) ->
-          Some (Format.sprintf "%s, %s" greeting name)
-        )
-      ;
-    ]
+let schema = Schema.(schema [
+    io_field "users"
+      ~args:Arg.[]
+      ~typ:(non_null (list (non_null user)))
+      ~resolve:(fun () () -> Lwt.return users)
+    ;
+    field "greeter"
+      ~typ:string
+      ~args:Arg.[
+        arg "config" ~typ:(non_null (obj "greeter_config" ~coerce:(fun greeting name -> (greeting, name)) ~fields:[
+          arg' "greeting" ~typ:string ~default:"hello";
+          arg "name" ~typ:(non_null string)
+        ]))
+      ]
+      ~resolve:(fun () () (greeting, name) ->
+        Some (Format.sprintf "%s, %s" greeting name)
+      )
+    ;
+  ]
 )
 
 let json_err = function
