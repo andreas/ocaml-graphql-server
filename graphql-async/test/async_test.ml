38babdd4319a081238b2feed5ac9c26f58affc14
Andreas Garnaes <andreas.garnaes@gmail.com>
2017-05-20 21:47:52 +0200

    Use yojson for test assertions

--- graphql-async/test/async_test.ml
+++ graphql-async/test/async_test.ml
@@ -2,6 +2,15 @@ open Graphql
 open Async_kernel
 open Async_unix
 
+let yojson = (module struct
+  type t = Yojson.Basic.json
+
+  let pp formatter t =
+    Format.pp_print_text formatter (Yojson.Basic.pretty_to_string t)
+
+  let equal = (=)
+end : Alcotest.TESTABLE with type t = Yojson.Basic.json)
+
 let test_query schema ctx query expected =
   Thread_safe.block_on_async_exn begin fun () ->
     match Graphql_parser.parse query with
@@ -12,7 +21,7 @@ let test_query schema ctx query expected =
       | Ok data -> data
       | Error err -> err
       in
-      Alcotest.(check string) "invalid execution result" expected (Yojson.Basic.to_string result')
+      Alcotest.check yojson "invalid execution result" expected result'
   end
 
 let schema = Graphql_async.Schema.(schema [
@@ -30,7 +39,12 @@ let schema = Graphql_async.Schema.(schema [
 
 let suite = [
   ("execution", `Quick, fun () ->
-    test_query schema () "{ direct_string io_int  }" "{\"data\":{\"direct_string\":\"foo\",\"io_int\":42}}"
+    test_query schema () "{ direct_string io_int  }" (`Assoc [
+      "data", `Assoc [
+        "direct_string", `String "foo";
+        "io_int", `Int 42
+      ]
+    ])
   );
 ]
 
--- graphql/test/schema_test.ml
+++ graphql/test/schema_test.ml
@@ -1,44 +1,109 @@
-open Test_common
+let test_query = Test_common.test_query Test_schema.schema ()
 
 let suite = [
   ("query", `Quick, fun () ->
     let query = "{ users { id } }" in
-    test_query Test_schema.schema () query "{\"data\":{\"users\":[{\"id\":1},{\"id\":2}]}}"
+    test_query query (`Assoc [
+      "data", `Assoc [
+        "users", `List [
+          `Assoc [
+            "id", `Int 1
+          ];
+          `Assoc [
+             "id", `Int 2
+          ]
+        ]
+      ]
+    ])
   );
   ("mutation", `Quick, fun () ->
     let query = "mutation { add_user(name: \"Charlie\", role: \"user\") { name } }" in
-    test_query Test_schema.schema () query "{\"data\":{\"add_user\":[{\"name\":\"Alice\"},{\"name\":\"Bob\"},{\"name\":\"Charlie\"}]}}"
+    test_query query (`Assoc [
+      "data", `Assoc [
+        "add_user", `List [
+          `Assoc [
+            "name", `String "Alice"
+          ];
+          `Assoc [
+            "name", `String "Bob"
+          ];
+          `Assoc [
+            "name", `String "Charlie"
+          ]
+        ]
+      ]
+    ])
   );
   ("__typename", `Quick, fun () ->
     let query = "{ __typename }" in
-    test_query Test_schema.schema () query "{\"data\":{\"__typename\":\"query\"}}"
+    test_query query (`Assoc [
+      "data", `Assoc [
+        "__typename", `String "query"
+      ]
+    ])
   );
   ("select operation (no operations)", `Quick, fun () ->
     let query = "fragment x on y { z }" in
-    test_query Test_schema.schema () query "{\"errors\":[{\"message\":\"No operation found\"}]}"
+    test_query query (`Assoc [
+      "errors", `List [
+        `Assoc [
+          "message", `String "No operation found"
+        ]
+      ]
+    ])
   );
   ("select operation (one operation, no operation name)", `Quick, fun () ->
     let query = "a { a: __typename }" in
-    test_query Test_schema.schema () query "{\"data\":{\"a\":\"query\"}}"
+    test_query query (`Assoc [
+      "data", `Assoc [
+        "a", `String "query"
+      ]
+    ])
   );
   ("select operation (one operation, matching operation name)", `Quick, fun () ->
     let query = "a { a: __typename }" in
-    test_query Test_schema.schema () query ~operation_name:"a" "{\"data\":{\"a\":\"query\"}}"
+    test_query query ~operation_name:"a" (`Assoc [
+      "data", `Assoc [
+        "a", `String "query"
+      ]
+    ])
   );
   ("select operation (one operation, missing operation name)", `Quick, fun () ->
     let query = "a { a: __typename }" in
-    test_query Test_schema.schema () query ~operation_name:"b" "{\"errors\":[{\"message\":\"Operation not found\"}]}"
+    test_query query ~operation_name:"b" (`Assoc [
+      "errors", `List [
+        `Assoc [
+          "message", `String "Operation not found"
+        ]
+      ]
+    ])
   );
   ("select operation (multiple operations, no operation name)", `Quick, fun () ->
     let query = "a { a: __typename } b { b: __typename }" in
-    test_query Test_schema.schema () query "{\"errors\":[{\"message\":\"Operation name required\"}]}"
+    test_query query (`Assoc [
+      "errors", `List [
+        `Assoc [
+          "message", `String "Operation name required"
+        ]
+      ]
+    ])
   );
   ("select operation (multiple operations, matching operation name)", `Quick, fun () ->
     let query = "a { a: __typename } b { b: __typename }" in
-    test_query Test_schema.schema () query ~operation_name:"b" "{\"data\":{\"b\":\"query\"}}"
+    test_query query ~operation_name:"b" (`Assoc [
+      "data", `Assoc [
+        "b", `String "query"
+      ]
+    ])
   );
   ("select operation (multiple operations, missing operation name)", `Quick, fun () ->
     let query = "a { a: __typename } b { b: __typename }" in
-    test_query Test_schema.schema () query ~operation_name:"c" "{\"errors\":[{\"message\":\"Operation not found\"}]}"
+    test_query query ~operation_name:"c" (`Assoc [
+      "errors", `List [
+        `Assoc [
+          "message", `String "Operation not found"
+        ]
+      ]
+    ])
   );
 ]
