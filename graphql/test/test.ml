738de77048e90ce6a1b34cfec584d804a913bf57
Andreas Garnaes <andreas.garnaes@gmail.com>
2017-09-11 15:25:21 +0200

    Move parser to separate OPAM package.

--- graphql/test/test.ml
+++ graphql/test/test.ml
@@ -1,6 +1,5 @@
 let () =
   Alcotest.run "graphql-server" [
-    "parser", Parser_test.suite;
     "schema", Schema_test.suite;
     "arguments", Argument_test.suite;
     "variables", Variable_test.suite;
