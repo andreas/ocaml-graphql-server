3fb8e14a8ab44ea4719bc64f004bc51574f289f2
Andreas Garnaes <andreas.garnaes@gmail.com>
2017-09-17 22:10:32 +0200

    Fix graphql tests
    A named query must be prefixed with 'query'

--- graphql/test/schema_test.ml
+++ graphql/test/schema_test.ml
@@ -18,27 +18,27 @@ let suite = [
     test_query Test_schema.schema () query "{\"errors\":[{\"message\":\"No operation found\"}]}"
   );
   ("select operation (one operation, no operation name)", `Quick, fun () ->
-    let query = "a { a: __typename }" in
+    let query = "query a { a: __typename }" in
     test_query Test_schema.schema () query "{\"data\":{\"a\":\"query\"}}"
   );
   ("select operation (one operation, matching operation name)", `Quick, fun () ->
-    let query = "a { a: __typename }" in
+    let query = "query a { a: __typename }" in
     test_query Test_schema.schema () query ~operation_name:"a" "{\"data\":{\"a\":\"query\"}}"
   );
   ("select operation (one operation, missing operation name)", `Quick, fun () ->
-    let query = "a { a: __typename }" in
+    let query = "query a { a: __typename }" in
     test_query Test_schema.schema () query ~operation_name:"b" "{\"errors\":[{\"message\":\"Operation not found\"}]}"
   );
   ("select operation (multiple operations, no operation name)", `Quick, fun () ->
-    let query = "a { a: __typename } b { b: __typename }" in
+    let query = "query a { a: __typename } query b { b: __typename }" in
     test_query Test_schema.schema () query "{\"errors\":[{\"message\":\"Operation name required\"}]}"
   );
   ("select operation (multiple operations, matching operation name)", `Quick, fun () ->
-    let query = "a { a: __typename } b { b: __typename }" in
+    let query = "query a { a: __typename } query b { b: __typename }" in
     test_query Test_schema.schema () query ~operation_name:"b" "{\"data\":{\"b\":\"query\"}}"
   );
   ("select operation (multiple operations, missing operation name)", `Quick, fun () ->
-    let query = "a { a: __typename } b { b: __typename }" in
+    let query = "query a { a: __typename } query b { b: __typename }" in
     test_query Test_schema.schema () query ~operation_name:"c" "{\"errors\":[{\"message\":\"Operation not found\"}]}"
   );
 ]
