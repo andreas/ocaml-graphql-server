3426f152cd31caa78788824b00404de4b1b63f91
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2021-02-14 11:33:21 -0800

    update nix sources

--- nix/sources.nix
+++ nix/sources.nix
@@ -1,18 +1,16 @@
-{ ocamlVersion ? "4_10" }:
-
+{ ocamlVersion ? "4_11" }:
 let
   overlays =
     builtins.fetchTarball
-      https://github.com/anmonteiro/nix-overlays/archive/5d6c844.tar.gz;
+      https://github.com/anmonteiro/nix-overlays/archive/69c0137.tar.gz;
 
 in
-
-  import "${overlays}/sources.nix" {
-    overlays = [
-      (import overlays)
-      (self: super: {
-        ocamlPackages = super.ocaml-ng."ocamlPackages_${ocamlVersion}".overrideScope'
-            (super.callPackage "${overlays}/ocaml" {});
-      })
-    ];
-  }
+import "${overlays}/sources.nix" {
+  overlays = [
+    (import overlays)
+    (self: super: {
+      ocamlPackages = super.ocaml-ng."ocamlPackages_${ocamlVersion}".overrideScope'
+        (super.callPackage "${overlays}/ocaml" { });
+    })
+  ];
+}
